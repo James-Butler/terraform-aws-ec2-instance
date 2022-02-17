83f5fc0a593214da63054564aa86da833a8bda21
James-Butler <james.butler@rovisys.com>
2022-02-16 17:46:42 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ locals {
   ami_owner                       = var.ami != "" ? var.ami_owner : join("", data.aws_ami.default.*.owner_id)
   root_volume_type                = var.root_volume_type != "" ? var.root_volume_type : data.aws_ami.info.root_device_type
   default_alarm_action            = var.default_alarm_action == null ? "": var.default_alarm_action
-  cloudwatch_alarm_action_enabled = var.default_alarm_action == null && local.enabled ? true : false
+  cloudwatch_alarm_action_enabled = var.default_alarm_action != null && local.enabled ? true : false
 
   region_domain  = local.region == "us-east-1" ? "compute-1.amazonaws.com" : "${local.region}.compute.amazonaws.com"
   eip_public_dns = "ec2-${replace(join("", aws_eip.default.*.public_ip), ".", "-")}.${local.region_domain}"
